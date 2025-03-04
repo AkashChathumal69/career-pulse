2c38c158af06edcf4f52e009b7cba59559d906a1
Lakshan Lakshitha <134108423+LakshanWanniarachchi@users.noreply.github.com>
2025-03-04 13:47:31 +0530

    create sign-up Gmail authentication

---
+++ lib/service/auth/auth_service.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  Future<UserCredential?> loginWithGmail() async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+
+      if (googleSignInAccount == null) {
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+
+      return await _auth.signInWithCredential(credential);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
