72faa6fe0074c5d3057b97a0cfea9e73dc037575
AkashChathumal69 <akashchathumal470630@gmail.com>
2025-03-29 13:50:03 +0530

    add category page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -28,7 +28,6 @@ class _LoginPageState extends State<LoginPage> {
       _isLoading = true;
 
       _authService.loginWithGmail(context);
-
     });
 
     try {
@@ -61,209 +60,197 @@ class _LoginPageState extends State<LoginPage> {
         //   title: Text('Career Pulse', style: TextStyle(color: Colors.black)),
         // ),
         body: Center(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 70, 0, 0),
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //logo
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 50),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 50),
 
-                      //Logo
-                      Image.asset('assets/logo.png', height: 100),
-                      SizedBox(height: 20),
+                    //Logo
+                    Image.asset('assets/logo.png', height: 120, width: 120),
+                    SizedBox(height: 20),
 
-                      // Topic
-                      Text(
-                        'Login Here',
-                        style: GoogleFonts.poppins(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+                    // Topic
+                    Text(
+                      'Login Here',
+                      style: GoogleFonts.poppins(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    // Email Fields
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 2,
+                          ),
+                        ),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    //Password Fields
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Colors.blueAccent,
+                            width: 2,
+                          ),
+                        ),
+                        labelText: 'Password',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+
+                    // Forgot Password
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Forgot your password?",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 3, 64, 170),
+                          ),
                         ),
                       ),
-                      SizedBox(height: 20),
+                    ),
+                    SizedBox(height: 10),
 
-                      // Email Fields
-                      TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
+                    // Login Button
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            3,
+                            64,
+                            170,
+                          ),
+                          shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.blueAccent,
-                              width: 2,
-                            ),
-                          ),
-                          labelText: 'Email',
-                        ),
-                      ),
-                      SizedBox(height: 20),
-
-                      //Password Fields
-                      TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            borderSide: BorderSide(
-                              color: Colors.blueAccent,
-                              width: 2,
-                            ),
-                          ),
-                          labelText: 'Password',
-                        ),
-                      ),
-                      SizedBox(height: 10),
-
-                      // Forgot Password
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            "Forgot your password?",
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 3, 64, 170),
-                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-
-                      // Login Button
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color.fromARGB(
-                              255,
-                              3,
-                              64,
-                              170,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: const Text(
-                            "Log In",
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          ),
+                        onPressed: () {},
+                        child: const Text(
+                          "Log In",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
                         ),
                       ),
-                      SizedBox(height: 20),
+                    ),
+                    SizedBox(height: 20),
 
-                      // create new account
-                      Text(
-                        "Don't have an account?",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 3, 64, 170),
-                          fontSize: 16,
+                    // create new account
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 3, 64, 170),
+                        fontSize: 16,
+                      ),
+                    ),
+
+                    //or log with
+                    SizedBox(height: 35),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Divider(color: Colors.grey[400], thickness: 1),
                         ),
-                      ),
 
-                      //or log with
-                      SizedBox(height: 35),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Divider(
-                              color: Colors.grey[400],
-                              thickness: 1,
-                            ),
-                          ),
+                        Text("or log in with"),
+                        Expanded(
+                          child: Divider(color: Colors.grey[400], thickness: 1),
+                        ),
+                      ],
+                    ),
 
-                          Text("or log in with"),
-                          Expanded(
-                            child: Divider(
-                              color: Colors.grey[400],
-                              thickness: 1,
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      //google button
-                      SizedBox(height: 35),
-                      Row(
-                        mainAxisAlignment:
-                            MainAxisAlignment.center, // Center the row content
-                        children: [
-                          Expanded(
-                            child: SizedBox(
-                              height: 50,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
+                    //google button
+                    SizedBox(height: 35),
+                    Row(
+                      mainAxisAlignment:
+                          MainAxisAlignment.center, // Center the row content
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              onPressed: _handleGoogleSignIn,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset('assets/google.png', height: 30),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    "Google",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15,
+                                    ),
                                   ),
-                                ),
-                                onPressed: _handleGoogleSignIn,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Image.asset(
-                                      'assets/google.png',
-                                      height: 30,
-                                    ),
-                                    SizedBox(width: 10),
-                                    Text(
-                                      "Google",
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 15,
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                                ],
                               ),
                             ),
                           ),
+                        ),
 
-                          SizedBox(width: 10),
+                        SizedBox(width: 10),
 
-                          // apple buttons
-                          Expanded(
-                            child: SizedBox(
-                              height: 50,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
+                        // apple buttons
+                        Expanded(
+                          child: SizedBox(
+                            height: 50,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
                                 ),
-                                onPressed: () {},
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Image.asset('assets/apple.png', height: 30),
-                                    SizedBox(width: 10),
-                                    Text(
-                                      "Apple",
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 15,
-                                      ),
+                              ),
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset('assets/apple.png', height: 30),
+                                  SizedBox(width: 10),
+                                  Text(
+                                    "Apple",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 15,
                                     ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
